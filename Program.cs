5c21f0e193b940cd3a38153c4cda5823845f3b9a
aishanoor1439 <aisha.noor1439@gmail.com>
2024-11-20 09:36:50 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distance_Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Initial velocity:");
+            double vi = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Final velocity:");
+            double vf = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter time duration:");
+            double t = Convert.ToDouble(Console.ReadLine());
+            double v = Convert.ToDouble(vf-vi);
+            double s = v * t;
+            Console.WriteLine("Distance(s) covered = " + s);
+
+        }
+    }
+}
